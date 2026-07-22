0ed20d416a8d379928b86318253a2d4f454a6325
Filip Stojanović <stojafil@fel.cvut.cz>
2026-07-22 11:44:40 +0200

    Remove -y from apt update (does nothing)

--- resources/scripts/remove_ppa.sh
+++ resources/scripts/remove_ppa.sh
@@ -6,7 +6,7 @@ sudo rm /etc/apt/sources.list.d/ctu-mrs-unstable.list
 sudo rm /etc/apt/preferences.d/ctu-mrs-unstable-preferences
 sudo rm /etc/ros/rosdep/sources.list.d/ctu-mrs-unstable.list
 
-sudo apt-get -y update
+sudo apt-get update
 rosdep update --include-eol-distros
 
 echo "$0: Finished removing MRS Unstable PPA repository"
