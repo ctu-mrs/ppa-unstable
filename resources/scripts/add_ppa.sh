36f10013b2e9572bb011351ef63cb2274687586c
Filip Stojanović <stojafil@fel.cvut.cz>
2026-07-27 13:41:55 +0200

    Make scripts more resilient

--- resources/scripts/add_ppa.sh
+++ resources/scripts/add_ppa.sh
@@ -7,15 +7,15 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "$0: Adding MRS Unstable PPA repository"
 
-sudo apt-get --no-install-recommends -y install curl gpg dpkg-dev
+sudo apt-get --no-install-recommends -y -o Acquire::Retries="4" install curl gpg dpkg-dev
 
 ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
 
-curl -s --compressed "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
-sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-apt.list"
-sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-unstable-preferences "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-ppa-preferences.txt"
-sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-$ARCH.list"
-sudo apt-get update
+curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
+sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-apt.list"
+sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/preferences.d/ctu-mrs-unstable-preferences "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-ppa-preferences.txt"
+sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/ros/rosdep/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-$ARCH.list"
+sudo apt-get -o Acquire::Retries="4" update
 
 rosdep update --include-eol-distros
 
